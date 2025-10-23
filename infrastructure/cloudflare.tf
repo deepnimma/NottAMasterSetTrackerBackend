4c9bc308f8eb0252ef70c1a27021fa4bcac07284
Deepesh Nimma <deepnimma@gmail.com>
2025-10-23 16:48:17 -0400

    Basic terraform

---
+++ infrastructure/cloudflare.tf
@@ -0,0 +1,10 @@
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+module "image_r2_bucket" {
+  source = "./modules/r2-bucket"
+
+  bucket_name = "tcg-image-bucket"
+  cloudflare_account_id = var.cloudflare_account_id
+}
---
+++ infrastructure/modules/r2-bucket/variables.tf
@@ -0,0 +1,8 @@
+variable "bucket_name" {
+  description = "The desired name for the R2 bucket."
+  type        = string
+}
+
+variable "cloudflare_account_id" {
+  description = "Your cloudflare account id"
+}
